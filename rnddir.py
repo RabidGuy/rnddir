9260b91f6f72a59ae7460851dbceb071a98a036c
Jack Stout <therabidguy@gmail.com>
2019-02-25 09:49:01 -0800

    Cleaned up code. Changed terms that implied context of original use to be more general.

--- rnddir.py
+++ rnddir.py
@@ -2,15 +2,32 @@
 #
 # - - - - - - - - - - - - - - - - - - -
 #
+# Update config file to JSON so that values can be stored in associative array.
+#
+# - - - - - - - - - - - - - - - - - - -
+#
 # Keep track of recent output in order to avoid sudden repeats.
 #
-#   Method 1) List last few choices in file equal to (length_of_listdir // 2).
-#   Generate list of directories *excluding* those in recents file.
+#   Use Method 3.
 #
-#   Method 2) List all known directories in file popping index zero for next
-#   map, then re-insert map into random position after mid-point.
+#       Method 1) List last few choices in file equal to (length_of_listdir // N).
+#       Generate list of directories *excluding* those in recents file.
+#
+#       Method 2) List all known directories in file popping index zero for next
+#       map, then re-insert map into random position after mid-point.
+#
+#       Method 3) List last few choices in file equal to (length_of_listdir // N).
+#       Attempt to select an unlisted, random directory a finite number of times.
+#       If no choice is made within that many attempts, use directory selected at
+#       last iteration.
 
 import os
+import random
+
+
+FILENAME_CONFIG = ".rnddircfg"
+# FILENAME_HISTORY = ".rnddirhist"
+# DEFAULT_HISTORY_LINES = 5
 
 
 def get_local_directories():
@@ -24,18 +41,22 @@ def get_local_directories():
     return directories
 
 
-if __name__ == "__main__":
-    from random import choice
+def choose_random_directory():
     ldirs = get_local_directories()
-    map_dir = choice(ldirs)
-    map_name = map_dir
+    return random.choice(ldirs) + os.sep
+
+
+if __name__ == "__main__":
+    directory = choose_random_directory()
     try:
-        with open(os.sep.join([map_dir, ".dircfg"])) as f:
+        with open(os.sep.join([directory, FILENAME_CONFIG])) as f:
             line = f.readline().strip()
             if line:
-                map_name = line + ("\n%s%s" % (map_name, os.sep))
+                out = line + ("\n%s" % directory)
             else:
-                map_name = "Invalid Name!" + ("\n%s%s" % (map_name, os.sep))
+                out = ("No name given in %s" %
+                       (directory + FILENAME_CONFIG)
+                      ) + ("\n%s" % directory)
     except FileNotFoundError:
-        pass
-    print(map_name)
+        out = directory
+    print(out)
